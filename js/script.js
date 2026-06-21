f05e68759399e65bad148fc62d29cb099f1d06ba
Sid Praveen <siddarthpraveen@icloud.com>
2026-06-21 15:39:18 -0400

    Update IAHSN website

--- js/script.js
+++ js/script.js
@@ -9,37 +9,38 @@ window.addEventListener('scroll', () => {
 
 
 
-var swiper = new Swiper(".slide-content", {
+if (document.querySelector(".slide-content") && window.Swiper) {
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
         slidesPerView: 3,
-        spaceBetween: 25,
-        loop: true,
-        centerSlide: 'true',
-        fade: 'true',
-        grabCursor: 'true',
-
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true, 
-          dynamicBullets: true,
-
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-
-        breakpoints:{
-            0: {
-                slidesPerView: 1,
-            },
-            520: {
-                slidesPerView: 2,
-            },
-            950: {
-                slidesPerView: 3,
-            },
-        },
-      });
+      },
+    },
+  });
+}
 
 
   const tabButtons = document.querySelectorAll(".tab-btn");
